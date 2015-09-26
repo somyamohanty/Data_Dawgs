a098dcd0e06e2dbd01446cf266398fce5d2c7453
Siddhant Sutar <siddhantsutar@yahoo.com>
2015-09-26 16:02:00 -0500

    Added movies list text file parser script.

---
+++ code/movies_list_parser.py
@@ -0,0 +1,17 @@
+"""
+movies_list_parser.py: Parses the list of iMDB titles obtained from
+ftp://ftp.fu-berlin.de/pub/misc/movies/database/movies.list.gz
+"""
+
+with open('../data/movies.txt', 'r') as fin:
+    data = fin.read().splitlines(True)
+
+# Remove the first 15 header lines, TV series (starts with a "), TV movies, and clean up the
+# individual lines to include only the movie title and its year.
+data = [x.split('\t')[0] + '\n' for x in data[15:-2] if not x.startswith('"') and '(TV)' not in x]
+
+with open('../data/parsed_movies.txt', 'w') as fout:
+    fout.writelines(data)
+
+fin.close()
+fout.close()
